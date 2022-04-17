0e4ba3b296cebdad7c46f6a00eedafdb6446949d
alesandi22 <alesf22@gmail.com>
2022-04-17 10:02:06 -0600

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Auth from '../views/Auth/Auth.vue';
 import Game from '../views/Game/Game.vue';
 
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
